c6d7d1b872dad48e25394571e2df237fcf545efe
Chrisvasa <Christophervasankari@gmail.com>
2022-12-22 14:00:22 +0100

    Base classes and menu almost done

---
+++ InputHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class InputHandler
+    {
+        ConsoleKey keyPressed;
+        ConsoleKey key1, key2, key3, key4;
+
+        public InputHandler()
+        {
+            //Allowed input
+            key1 = ConsoleKey.UpArrow;
+            key2 = ConsoleKey.DownArrow;
+            key3 = ConsoleKey.Enter;
+            key4 = ConsoleKey.Spacebar;
+        }
+        // Asks for input until valid, then returns it
+        public ConsoleKey ReadInput()
+        {
+            do
+            {
+                keyPressed = Console.ReadKey(true).Key;
+            } while (keyPressed != key1 && keyPressed != key2 && keyPressed != key3 && keyPressed != key4);
+            return keyPressed;
+        }
+    }
+}
---
+++ Login.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class Login
+    {
+        protected string[,] users = new string[0, 0];
+        public Login() 
+        {
+            string[] userArr = File.ReadAllLines(@"C:\Users\Chris\Desktop\Bank\Users.txt");
+            users = new string[userArr.Count(), 2];
+
+            for (int i = 0; i < userArr.Count(); i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    var numbers = userArr[i].Split(',');
+                    users[i, j] = numbers[j];
+                    if (j == 1)
+                    {
+                        users[i, j] = users[i, j].TrimStart();
+                    }
+                }
+            }
+        }
+
+        public void UserLogin()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter your username: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            string password = Console.ReadLine();
+        }
+    }
+}
